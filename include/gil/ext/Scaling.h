947c9f63102d15cc182099851cacd6dd7128b172
Tz-Huan Huang <tzhuan@gmail.com>
2006-11-27 21:16:12 +0000

    * add TrivialScaler (tested).
    git-svn-id: file:///svn/cmlib/trunk@115 25493b3f-faf5-0310-b690-0363d7aa3cd8

--- include/gil/ext/Scaling.h
+++ include/gil/ext/Scaling.h
@@ -1,22 +1,54 @@
 #ifndef SCALING_H
 #define SCALING_H
 
+#include <iostream>
+using namespace std;
+
+#include <algorithm>
+
 #include "Filterer.h"
 
 namespace std {
 	template<typename I, typename F>
-	void scale(I &dst, const I &src, const F &filter)
+	void scale(I &dst, const I &src, const F &filterer)
 	{
-		filter(dst, src, filter);
+		filter(dst, src, filterer);
 	}
 
 	template<typename I, typename F>
-	I scale(const I &src, const F &filter, size_t width, size_t height)
+	I scale(const I &src, const F &filterer, size_t width, size_t height)
 	{
 		I dst(width, height);
-		scale(dst, src, filter);
+		scale(dst, src, filterer);
 		return dst;
 	}
+
+	template<typename I, typename T = double>
+	class TrivialScaler {
+		public:
+			typedef typename I::value_type value_type;
+
+			TrivialScaler(T x, T y): my_ratio_x(x), my_ratio_y(y)
+			{
+			}
+
+			value_type operator()(const I &image, int x, int y) const 
+			{
+				int _x = static_cast<int>( x/my_ratio_x + 0.5 );
+				int _y = static_cast<int>( y/my_ratio_y + 0.5 );
+
+				_x = std::min( _x, static_cast<int>(image.width())-1 );
+				_x = std::max( _x, 0 );
+
+				_y = std::min( _y, static_cast<int>(image.height())-1 );
+				_y = std::max( _y, 0 );
+
+				return image(_x, _y);
+			}
+		private:
+			T my_ratio_x;
+			T my_ratio_y;
+	};
 }
 
 #endif
