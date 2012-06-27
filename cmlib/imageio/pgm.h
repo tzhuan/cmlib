fceb7cf1253e6208cecf3b7be1d2042650f89d13
Tz-Huan Huang <tzhuan@gmail.com>
2012-06-27 14:12:49 +0800

    Add license header to all files.

--- cmlib/imageio/pgm.h
+++ cmlib/imageio/pgm.h
@@ -1,1 +1,8 @@
+/*
+Copyright NTU CSIE CMLAB 2005 - 2012
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file ../../LICENSE_1_0.txt or copy at
+http://www.boost.org/LICENSE_1_0.txt)
+*/
+
 #define CMLIB_IMAGE_IO_PGM
